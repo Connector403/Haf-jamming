229ce3e9fffb96ca13d50b39bc630e9414794d4d
Connector403 <44946843+Connector403@users.noreply.github.com>
2020-09-03 15:35:42 +0100

    Still there are a few improvements, but this works for now.

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Playlist.css';
+import TrackList from '../TrackList/TrackList';
+
+class PlayList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+    handleNameChange(event) {
+        this.props.onNameChange(event.target.value);
+    }
+    render() {
+        return (
+            <div className="Playlist">
+                <input defaultValue={'New playlist'} onChange={this.handleNameChange} />
+                <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true } />
+               
+                <button class="Playlist-save" onClick={ this.props.onSave}>SAVE TO SPOTIFY</button>
+            </div>
+            ); // return 
+    }
+}
+
+
+export default PlayList;
---
+++ src/util/Spotify.js
@@ -0,0 +1,83 @@
+let accessToken;
+const clientId = 'fcd402399c3c4b78a753f6c468157861';
+const redirectUri = 'http://poised-brain.surge.sh';
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        }
+
+        // get access token 
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+        }
+
+    }, // .getAccessToken()
+
+    search(term) {
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            }).then(respone => {
+                return respone.json();
+            }).then(jsonResponse => {
+                if (!jsonResponse.tracks) {
+                    return [];
+                }
+                return jsonResponse.tracks.items.map(track => ({
+                    id: track.id,
+                    name: track.name,
+                    artist: track.artists[0].name,
+                    album: track.album.name,
+                    uri: track.uri
+                }));
+            });
+
+    },
+
+    savePlaylist(name, trackUris) {
+        if (!name || !trackUris.length) {
+            return;
+        }
+
+        const accessToken = Spotify.getAccessToken();
+        const headers = { Authorization: `Bearer ${accessToken}` };
+        let userId;
+
+        return fetch('https://api.spotify.com/v1/me', { headers: headers }
+        ).then(response => { return response.json() }
+        ).then(jsonResponse => {
+            userId = jsonResponse.id;
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,
+                {
+                    headers: headers,
+                    method: 'POST',
+                    body: JSON.stringify({ name: name })
+                }).then(response => response.json()
+            ).then(jsonResponse => {
+                const playlistId = jsonResponse.id;
+                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+                    headers: headers,
+                    method: 'POST',
+                    body: JSON.stringify({ uris: trackUris })
+                })
+            })
+
+        })
+    }
+}
+
+export default Spotify;
